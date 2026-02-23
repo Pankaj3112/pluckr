9e6663f459a60878e849e43ade930307984e887d
Pankaj Beniwal <pankajbeniwal3112@gmail.com>
2026-02-23 17:24:22 +0530

    feat: export FieldMapping and FieldMappings types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { Scraper, type ScraperConfig } from './scraper.js'
 export { ExtractionFailed, PermanentFailure } from './exceptions.js'
+export type { FieldMapping, FieldMappings } from './types.js'
