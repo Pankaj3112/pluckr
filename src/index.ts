50aaf16f81faa3d9c98b326740630504dc41d8ee
Pankaj Beniwal <pankajbeniwal3112@gmail.com>
2026-02-23 19:42:32 +0530

    feat: export cleanHtml utility from index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Scraper, type ScraperConfig } from './scraper.js'
+export { cleanHtml } from './cleaner.js'
 export { ExtractionFailed, PermanentFailure } from './exceptions.js'
 export type { FieldMapping, FieldMappings, ScrapeResult, ScrapeError } from './types.js'
